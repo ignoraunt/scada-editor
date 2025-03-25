681f8caa4d926f4aa361beb974b8bf991262ce84
ignoraunt <doryphore@zoho.com>
2025-03-25 10:17:01 +0300

    Added "yet unusable" utils

--- modules/elements-resizing.js
+++ modules/elements-resizing.js
@@ -1,18 +1,8 @@
 var l = console.log;
 
-export function elementsResizing() {
-  var throttle = (callee, delay) => {
-    var timeout = 0;
-    return (...args) => {
-      if (timeout !== 0) return;
-      timeout = setTimeout(() => {
-        callee(...args);
-        clearTimeout(timeout);
-        timeout = 0;
-      }, delay);
-    };
-  };
+import { utils } from "./overarching-utilities.js";
 
+export function elementsResizing() {
   var currentBlock = null;
   var targetResizeAxis = "";
 
@@ -144,7 +134,7 @@ export function elementsResizing() {
     }, 35);
   }
 
-  var handleMouseMoveThrottled = throttle(detectElementEdge, 10);
+  var handleMouseMoveThrottled = utils.throttle(detectElementEdge, 10);
 
   function mm(e, targetBlock) {
     targetBlock.style.width = e.x + "px";
---
+++ modules/overarching-utilities.js
@@ -0,0 +1,13 @@
+export var utils = {
+  throttle: function (callee, delay) {
+    var timeout = 0;
+    return (...args) => {
+      if (timeout !== 0) return;
+      timeout = setTimeout(() => {
+        callee(...args);
+        clearTimeout(timeout);
+        timeout = 0;
+      }, delay);
+    };
+  },
+};
