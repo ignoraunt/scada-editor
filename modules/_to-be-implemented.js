5a99192104f0485f8b7a373d57fd708eb83e9546
ignoraunt <doryphore@zoho.com>
2025-04-03 11:41:56 +0300

    Add notes file

---
+++ modules/_to-be-implemented.js
@@ -0,0 +1,23 @@
+// if (args.action === "picture") {
+//   insertPicture(args);
+//   return;
+// }
+
+// function insertPicture() {
+//   var img = document.createElement("img");
+//   img.src = "./turbine.jpg";
+//   var src = document.createElement("div");
+//   src.style.position = "absolute";
+//   src.style.left = "200px";
+//   src.style.top = "200px";
+//   src.append(img);
+//   document.body.append(src);
+// }
+
+// ==== ==== ==== ==== ==== ==== ==== ====
+
+// function copyBlock(args) {
+//   //
+// }
+
+// ==== ==== ==== ==== ==== ==== ==== ====
